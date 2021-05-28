c71a708a9e85847f3467546b70d0d06c03550caf
MixFon <mixfon@rambler.ru>
2021-05-28 21:47:40 +0300

    add step three

--- UIRubic/UIRubic/Solution.swift
+++ UIRubic/UIRubic/Solution.swift
@@ -17,7 +17,8 @@ class Solution {
     
     func solution() -> Cube {
         stepFirst()
-        secondStep()
+        stepSecond()
+        stepThree()
         return self.cube
     }
     
@@ -184,22 +185,22 @@ class Solution {
     
     //MARK: Второй этап. Первый слой.
     // Перебор граней от 0 до 3. [2, 20, 18, 0] - номера кубиков, которе должны переместить.
-    private func secondStep() {
+    private func stepSecond() {
         for (face, number) in zip(self.cube.faces[0...3], [2, 20, 18, 0]) {
-            if isNumberInUp(number: number) {
-                moveNumber(face: face, number: number)
+            if isNumberInCornerUp(number: number) {
+                moveNumberToCorner(face: face, number: number)
                 moveNuberToDown(face: face, number: number)
             } else {
                 let faceOnePifPaf = moveNuberFromDown(number: number)!
-                pifPaf(fase: faceOnePifPaf)
-                moveNumber(face: face, number: number)
+                pifPafRighr(fase: faceOnePifPaf)
+                moveNumberToCorner(face: face, number: number)
                 moveNuberToDown(face: face, number: number)
             }
         }
     }
     
-    // Комбинация пиф-паф (относительно текущей грани R U R' U')
-    private func pifPaf(fase: Face) {
+    // Комбинация пиф-паф (относительно текущей грани face R U R' U')
+    private func pifPafRighr(fase: Face) {
         let flipRight = fase.flip.faceRight()!
         self.cube.flip(flipRight)
         self.cube.flip(.U)
@@ -207,8 +208,17 @@ class Solution {
         self.cube.flip(._U)
     }
     
-    // Проверка находится ли номер в верхнем слое (где Up)
-    private func isNumberInUp(number: Int) -> Bool {
+    // Комбинация пиф-паф влево (относительно текущей грани face L' U' L U)
+    private func pifPafLeft(fase: Face) {
+        let flipLeft = fase.flip.faceLeft()!
+        self.cube.flip(flipLeft.faceOpposite()!)
+        self.cube.flip(._U)
+        self.cube.flip(flipLeft)
+        self.cube.flip(.U)
+    }
+    
+    // Проверка находится ли номер в верхнем слое в УГЛАХ (где Up)
+    private func isNumberInCornerUp(number: Int) -> Bool {
         return
             self.cube.numbers[0][2][0] == Int8(number) ||
             self.cube.numbers[0][2][2] == Int8(number) ||
@@ -216,8 +226,18 @@ class Solution {
             self.cube.numbers[2][2][0] == Int8(number)
     }
     
+    // Проверка находится ли номер в верхнем слое в ЦЕНТРЕ (где Up)
+    private func isNumberInCentreUp(number: Int) -> Bool {
+        return
+            self.cube.numbers[0][2][1] == Int8(number) ||
+            self.cube.numbers[1][2][2] == Int8(number) ||
+            self.cube.numbers[2][2][1] == Int8(number) ||
+            self.cube.numbers[1][2][0] == Int8(number)
+    }
+    
+    
     // Передвижение номера в правый верхний угол заданной грани. Координаты этих углов.
-    private func moveNumber(face: Face, number: Int) {
+    private func moveNumberToCorner(face: Face, number: Int) {
         let coordinate: (Int, Int, Int)
         switch face.flip {
         case .L:
@@ -254,7 +274,7 @@ class Solution {
             coordinate = (0, 0, 0)
         }
         while self.cube.faces[face.index].matrix[2][2] != face.color || self.cube.numbers[coordinate.0][coordinate.1][coordinate.2] != number {
-            pifPaf(fase: face)
+            pifPafRighr(fase: face)
         }
     }
     
@@ -274,4 +294,55 @@ class Solution {
         }
     }
     
+    // MARK: Этап третий. Средний слой.
+    private func stepThree() {
+        forFlipRighr()
+        forFlipLeft()
+    }
+    
+    // Для поворота из центральной верхней вправо.
+    private func forFlipRighr() {
+        for (face, number) in zip(self.cube.faces[0...3], [5, 23, 21, 3]) {
+            if isNumberInCentreUp(number: number) {
+                moveNumberToСentre(face: face, number: number)
+                self.cube.flip(.U)
+                pifPafRighr(fase: face)
+                pifPafLeft(fase: self.cube.faces[(face.index + 1) % 4])
+            }
+        }
+    }
+    
+    // Для поворота из центральной верхней влево.
+    private func forFlipLeft() {
+        for (face, number) in zip(self.cube.faces[0...3], [3, 5, 23, 21]) {
+            if isNumberInCentreUp(number: number) {
+                moveNumberToСentre(face: face, number: number)
+                self.cube.flip(._U)
+                pifPafLeft(fase: face)
+                let index = (face.index - 1) < 0 ? 4 : face.index - 1
+                pifPafRighr(fase: self.cube.faces[index])
+            }
+        }
+    }
+    
+    // Передвижение номера в центры верхней грани. Координаты этих углов.
+    private func moveNumberToСentre(face: Face, number: Int) {
+        let coordinate: (Int, Int, Int)
+        switch face.flip {
+        case .L:
+            coordinate = (0, 2, 1)
+        case .F:
+            coordinate = (1, 2, 2)
+        case .R:
+            coordinate = (2, 2, 1)
+        case .B:
+            coordinate = (1, 2, 0)
+        default:
+            print("Error move number to centre.")
+            coordinate = (0, 0, 0)
+        }
+        while self.cube.numbers[coordinate.0][coordinate.1][coordinate.2] != number {
+            self.cube.flip(.U)
+        }
+    }
 }
